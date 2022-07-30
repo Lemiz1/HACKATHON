424e97ad031783a28e5fc29067a06315bd7aadad
Lemiz1 <khanhdvfs91471@fpt.edu.vn>
2022-07-30 23:38:55 +0700

    adding files.-m

---
+++ src/hackathon_frontend/src/index.jsx
@@ -0,0 +1,42 @@
+import { hackathon_backend } from "../../declarations/hackathon_backend";
+import * as React from "react";
+import { render } from "react-dom";
+import "/home/dokhanh/hackathon/bulma.min.css";
+
+
+const MyHello = () => {
+  const [name, setName] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  async function doGreet() {
+    const greeting = await hackathon_backend.greet(name);
+    setMessage(greeting);
+  }
+
+  return (
+    <div style={{ "fontSize": "30px" }}>
+      <div style={{ "backgroundColor": "yellow" }}>
+        <p>Greetings, from DFINITY!</p>
+        <p>
+          {" "}
+          Type your message in the Name input field, then click{" "}
+          <b> Get Greeting</b> to display the result.
+        </p>
+      </div>
+      <div style={{ margin: "30px" }}>
+        <input
+          id="name"
+          value={name}
+          onChange={(ev) => setName(ev.target.value)}
+        ></input>
+        <button onClick={doGreet}>Get Greeting!</button>
+      </div>
+      <div>
+        Greeting is: "
+        <span style={{ color: "blue" }}>{message}</span>"
+      </div>
+    </div>
+  );
+};
+
+render(<MyHello />, document.getElementById("app"));
